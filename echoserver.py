c5c1508e8f1e3385dd768c1574cf192ee7a6b7dc
Kai <apprentice.zk@gmail.com>
2013-09-01 09:04:03 +0800

    Client.py and server.py

--- echoserver.py
+++ echoserver.py
@@ -23,7 +23,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
 s.listen(1)
-print "Parent at %d listening for connections" %os.getpid()
+print "Parent at %d listening for connections!" %os.getpid()
 while 1:
      try:
         clientsock, clientaddr = s.accept()
